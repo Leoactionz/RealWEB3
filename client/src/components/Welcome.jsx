b087f46ef16f376a17551c80548b63a735a7eedf
leo <leoucu@gmail.com>
2022-04-21 02:31:32 +0100

    added comment

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -131,7 +131,7 @@ const Welcome = () => {
 
             <div className='h-[1px] w-full bg-gray-400 my-2' />
 
-            {isLoading ? (
+            {isLoading ? ( // Added loader
               <Loader />
             ) : (
               <button
